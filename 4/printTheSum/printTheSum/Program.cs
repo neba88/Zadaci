36cc80e9a0c03bddc27079b3ac7b75633b00a80e
Nebojsa Kalanj <nebojsa.kalanj.developer@gmail.com>
2014-12-16 14:42:09 +0100

    dodat 4 zadatak

---
+++ 4/printTheSum/printTheSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace printTheSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estetika est = new Estetika();
+            est.Look();
+
+            int i, sum = 0, n;
+
+            Console.Write("\n\tEnter a number : ");
+            n = int.Parse(Console.ReadLine());
+            
+            for (i = 0; i <= n; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("\n\t---------------------------");
+            Console.WriteLine("\n\tSum of all numbers : " + sum);
+            Console.ReadLine();
+        }
+    }
+}

c3a1617e97e55aa093955ccea84555ba9196c36e
neba88 <nebojsa.kalanj.developer@gmail.com>
2014-12-16 16:13:35 +0100

    dodao i peti zadatak

--- 5/petiZadatak/petiZadatak/Estetika.cs
+++ 5/petiZadatak/petiZadatak/Estetika.cs
@@ -1,10 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace petiZadatak
+{
+    class Estetika
+    {
         public void Look()
         {
             Console.BackgroundColor = ConsoleColor.White;
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Black;
-            Console.Title = "Zadatak";
+            Console.Title = "Zadatak 5";
         }
-
-	Estetika est = new Estetika();
-        est.Look();
+    }
+}
---
+++ 5/petiZadatak/petiZadatak/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace petiZadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estetika est = new Estetika();
+            est.Look();
+
+            int n = -1;
+            int sum = 3;
+
+            Console.Write("\n\t Enter a number that is >3 : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n\t ---------------------------------");
+            Console.Write("\n\n\t 3");
+
+            for (int i = 5; i <= n; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    Console.Write (" + " + i);
+                    sum += i;
+                }
+            }
+            Console.WriteLine(" = " + sum);
+            Console.ReadLine();
+        }
+    }
+}

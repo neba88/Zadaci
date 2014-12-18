d93a16ae6dda133567b556d0bf96e351914e2c42
neba88 <nebojsa.kalanj.developer@gmail.com>
2014-12-18 23:42:29 +0100

    sesti zadatak

---
+++ 6/sestiZadatak/sestiZadatak/Estetika.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sestiZadatak
+{
+    class Estetika
+    {
+        public void Look()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Title = "Zadatak";
+        }
+    }
+}
---
+++ 6/sestiZadatak/sestiZadatak/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sestiZadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estetika est = new Estetika();
+            est.Look();
+
+            Console.Write("\n\tPlease enter a number greater than 1: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\tType 'zbir' or 'niz' to see the sum or the product: ");
+            string ans = Console.ReadLine();
+
+            if (ans.Equals("zbir"))
+            {
+                Console.WriteLine("\tThe total sum is " + ((n + 1) * (n) / 2));
+            }
+            else if (ans.Equals("niz"))
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write("{0} ", i);
+                }
+                Console.WriteLine("\n\tfor N is: " + n);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

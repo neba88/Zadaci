654262120f6e528f9e74f99eb4d5ec0cf716dac1
neba88 <nebojsa.kalanj.developer@gmail.com>
2014-12-18 23:57:03 +0100

    sedmi zadatak

---
+++ 7/sedmiZadatak/sedmiZadatak/Estetika.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sedmiZadatak
+{
+    class Estetika
+    {
+        public void Look()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Title = "Zadatak 7";
+        }
+    }
+}
---
+++ 7/sedmiZadatak/sedmiZadatak/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sedmiZadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estetika est = new Estetika();
+            est.Look();
+
+            for (int i = 1; i <= 12; i++)
+            {
+                for (int j = 1; j <= 12; j++)
+                {
+                    string print = string.Format("\t{0}", i * j);
+                    Console.Write(print);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}

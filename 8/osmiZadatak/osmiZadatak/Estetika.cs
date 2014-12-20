20b4a841f0ca1aa8b93ba2c8725c7484be787f2a
Nebojsa Kalanj <nebojsa.kalanj.developer@gmail.com>
2014-12-20 01:17:55 +0100

    osmi zadatak

---
+++ 8/osmiZadatak/osmiZadatak/Estetika.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace osmiZadatak
+{
+    class Estetika
+    {
+        public void Look()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Title = "Zadatak 8";
+        }
+
+
+    }
+}
---
+++ 8/osmiZadatak/osmiZadatak/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace osmiZadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estetika est = new Estetika();
+            est.Look();
+
+            Console.WriteLine("\n\t--- Primes 0 to 100 ---");
+            Console.WriteLine("\n\t--- Press enter to print ---");
+            Console.ReadLine();
+            for (int i = 0; i < 100; i++)
+            {
+                bool primeBroj = PrimeTool.IsPrime(i);
+                if (primeBroj)
+	            {
+                    Console.Write("{0} ", i);
+	            }
+            }
+            Console.WriteLine("\n\n\t--- Press enter to exit ---");
+            Console.ReadLine();
+        }
+    }
+}
